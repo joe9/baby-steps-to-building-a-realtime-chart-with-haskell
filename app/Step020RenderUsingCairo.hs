f7081d49c00c7dad802b8256b26742c7203eab50
joe9 <joe9mail@gmail.com>
2016-09-06 11:36:41 -0700

    formatted code

--- app/Step020RenderUsingCairo.hs
+++ app/Step020RenderUsingCairo.hs
@@ -5,15 +5,15 @@
 
 module Main where
 
-import Graphics.UI.Gtk hiding (rectangle,Scale)
-import Diagrams.Prelude             hiding (dot, frame, set)
 import Diagrams.Backend.Cairo
 import Diagrams.Backend.Cairo.Internal
-import Graphics.Rendering.Cairo hiding (scale)
+import Diagrams.Prelude                hiding (dot, frame, set)
+import Graphics.Rendering.Cairo        hiding (scale)
+import Graphics.UI.Gtk                 hiding (Scale, rectangle)
 --
+import Axis
 import PriceGraph
 import Scale
-import Axis
 import VolumeGraph
 
 data MyData =
@@ -59,34 +59,35 @@ frame = (showOrigin . lineWidth ultraThin . rect frameWidth) frameHeight
 
 pChart :: QDiagram B V2 Double Any
 pChart =
---   showEnvelope
-    (priceGraph xScale
-             priceScale
-             (map (\d -> (mdId d,mdBid d)) dataSeries)
-             (map (\d -> (mdId d,mdAsk d)) dataSeries))
+  --   showEnvelope
+  (priceGraph xScale
+              priceScale
+              (map (\d -> (mdId d,mdBid d)) dataSeries)
+              (map (\d -> (mdId d,mdAsk d)) dataSeries))
 
 vChart :: QDiagram B V2 Double Any
 vChart =
---   showEnvelope
-   (volumeGraph xScale
-              volumeScale
-              (map (\d -> (mdId d,mdVolume d)) dataSeries))
+  --   showEnvelope
+  (volumeGraph xScale
+               volumeScale
+               (map (\d -> (mdId d,mdVolume d)) dataSeries))
 
 chart :: QDiagram B V2 Double Any
 chart =
   position [(p2 (frameWidth / 2,frameHeight / 2),frame)
-           ,(p2 (margin,margin+volumeChartHeight)
-            , pChart)
-           ,(p2 (margin,margin)
-            , vChart)
+           ,(p2 (margin,margin + volumeChartHeight),pChart)
+           ,(p2 (margin,margin),vChart)
            ,(p2 (frameWidth / 2,margin),bottomAxis xScale)
-           ,(p2 (frameWidth / 2,margin+volumeChartHeight),bottomAxis xScale)
+           ,(p2 (frameWidth / 2,margin + volumeChartHeight),bottomAxis xScale)
            ,(p2 (frameWidth / 2,frameHeight - margin),topAxis xScale)
-           ,(p2 (margin,margin + volumeChartHeight + (priceChartHeight / 2)),leftAxis priceScale)
+           ,(p2 (margin,margin + volumeChartHeight + (priceChartHeight / 2))
+            ,leftAxis priceScale)
            ,(p2 (margin,margin + (volumeChartHeight / 2)),leftAxis volumeScale)
-           ,(p2 (frameWidth - margin,margin + volumeChartHeight + (priceChartHeight / 2)),rightAxis priceScale)
-           ,(p2 (frameWidth - margin,margin + (volumeChartHeight / 2)),rightAxis volumeScale)
-           ]
+           ,(p2 (frameWidth - margin
+                ,margin + volumeChartHeight + (priceChartHeight / 2))
+            ,rightAxis priceScale)
+           ,(p2 (frameWidth - margin,margin + (volumeChartHeight / 2))
+            ,rightAxis volumeScale)]
 
 -- The size of the chart, in logical units. All the diagrams use the
 --  logical units. The translation from the actual units to the logical
@@ -108,34 +109,44 @@ priceChartHeight = 800
 
 volumeChartHeight = 200
 
-renderDiagram :: Int -> Int -> Diagram Cairo -> Graphics.Rendering.Cairo.Render ()
+renderDiagram
+  :: Int -> Int -> Diagram Cairo -> Graphics.Rendering.Cairo.Render ()
 renderDiagram w h c =
-  snd (renderDia Cairo ((CairoOptions "" (mkSizeSpec2D (Just (fromIntegral w)) (Just (fromIntegral h))) RenderOnly False) :: Options Cairo V2 Double) c)
+  snd (renderDia Cairo
+                 ((CairoOptions
+                     ""
+                     (mkSizeSpec2D (Just (fromIntegral w))
+                                   (Just (fromIntegral h)))
+                     RenderOnly
+                     False) :: Options Cairo V2 Double)
+                 c)
 
 main :: IO ()
-main= do
-     _ <- initGUI
+main =
+  do _ <- initGUI
      window <- windowNew
      let rows = 30
          columns = 11
      table <- tableNew rows columns True
-     set window [windowTitle := "Hello Cairo", containerChild := table]
-
+     set window [windowTitle := "Hello Cairo",containerChild := table]
      canvas <- drawingAreaNew
-     tableAttachDefaults table canvas 0 columns 0 (rows - 3)
-     widgetModifyBg canvas StateNormal (Color 65535 65535 65535)
-
+     tableAttachDefaults table
+                         canvas
+                         0
+                         columns
+                         0
+                         (rows - 3)
+     widgetModifyBg canvas
+                    StateNormal
+                    (Color 65535 65535 65535)
      widgetShowAll window
---      drawin <- widgetGetDrawWindow canvas
-     _ <- onExpose canvas (\_ ->  do
-                                (w,h) <- widgetGetSize canvas
-                                drawin <- widgetGetDrawWindow canvas
---                                 renderWithDrawable drawin renderFigure
-                                renderWithDrawable drawin (renderDiagram w h chart)
---                                 renderWithDrawable drawin
---                                     (myDraw (fromIntegral w)(fromIntegral h))
-                                return True)
---      (canvas `on` exposeEvent) renderFigure
-
+     _ <-
+       onExpose canvas
+                (\_ ->
+                   do (w,h) <- widgetGetSize canvas
+                      drawin <- widgetGetDrawWindow canvas
+                      renderWithDrawable drawin
+                                         (renderDiagram w h chart)
+                      return True)
      _ <- onDestroy window mainQuit
      mainGUI
