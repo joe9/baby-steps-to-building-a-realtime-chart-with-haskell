ab7e112e9bfc061bad3d59c5615ffdb14ffc3796
joe9 <joe9mail@gmail.com>
2016-09-08 14:21:43 -0700

    moved data to a different module

---
+++ app/MyData.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE GADTs                     #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE PartialTypeSignatures     #-}
+{-# LANGUAGE TypeFamilies              #-}
+
+module MyData where
+
+import           Control.Monad
+import           System.Random
+import           Data.List
+import qualified Data.IntMap.Strict                  as IntMap
+
+data MyData =
+  MyData {mdId     :: Int
+         ,mdBid    :: Double
+         ,mdAsk    :: Double
+         ,mdVolume :: Double}
+  deriving (Eq,Read,Show)
+
+staticDataSeries :: IntMap.IntMap MyData
+staticDataSeries =
+  (IntMap.fromList . map (\d -> (mdId d,d)))
+    [MyData 1 1.19 1.26 1000
+    ,MyData 2 1.22 1.27 2000
+    ,MyData 3 1.27 1.37 1000
+    ,MyData 4 1.37 1.47 0
+    ,MyData 5 1.67 1.97 3000
+    ,MyData 6 1.57 1.67 1000
+    ,MyData 7 1.47 1.57 1000
+    ,MyData 8 1.27 1.37 500
+    ,MyData 9 1.17 1.25 5000
+    ,MyData 10 1.1 1.15 0]
+
+buildDataSeries :: IO ( IntMap.IntMap MyData)
+buildDataSeries = do
+  let numberOfElements = 10000
+  bids <- replicateM numberOfElements (randomRIO (1,2))
+  asks <- replicateM numberOfElements (randomRIO (2,3))
+  volumes <- replicateM numberOfElements (randomRIO (0,1000000))
+  (return . IntMap.fromList . map (\d -> (mdId d,d))) (zipWith4 MyData [1..] bids asks volumes)
--- app/Step025Gloss.hs
+++ app/Step025Gloss.hs
@@ -6,6 +6,9 @@
 module Main where
 
 import           Control.Concurrent
+import           Control.Monad
+import           System.Random
+import           Data.List
 import           Control.Concurrent.Async
 import qualified Data.IntMap.Strict                  as IntMap
 import           Data.IORef
@@ -13,27 +16,7 @@ import           Graphics.Gloss.Interface.IO.Animate
 --
 import ChartGloss
 import TypesGloss
-
-data MyData =
-  MyData {mdId     :: Int
-         ,mdBid    :: Double
-         ,mdAsk    :: Double
-         ,mdVolume :: Double}
-  deriving (Eq,Read,Show)
-
-dataSeries :: IntMap.IntMap MyData
-dataSeries =
-  (IntMap.fromList . map (\d -> (mdId d,d)))
-    [MyData 1 1.19 1.26 1000
-    ,MyData 2 1.22 1.27 2000
-    ,MyData 3 1.27 1.37 1000
-    ,MyData 4 1.37 1.47 0
-    ,MyData 5 1.67 1.97 3000
-    ,MyData 6 1.57 1.67 1000
-    ,MyData 7 1.47 1.57 1000
-    ,MyData 8 1.27 1.37 500
-    ,MyData 9 1.17 1.25 5000
-    ,MyData 10 1.1 1.15 0]
+import MyData
 
 -- http://code.haskell.org/gtk2hs/docs/tutorial/Tutorial_Port/app1.xhtml
 -- http://stackoverflow.com/questions/26848694/cairo-flips-a-drawing
@@ -52,25 +35,19 @@ renderChart ref w h _ =
                    scale 0.1 0.1 . text . show . IntMap.findMax) series])
 
 main :: IO ()
-main =
-  do ref <- newIORef dataSeries
-     a <- async (threadDelay (1 * 1000 * 1000) >> updatedData ref dataSeries)
-     let w = 100
-         h = 100
-     animateIO (InWindow "Realtime Chart"
+main = do
+  dataSeries <- buildDataSeries
+  ref <- newIORef dataSeries
+  a <- async (threadDelay (1 * 1000 * 1000) >> updatedData ref dataSeries)
+  let w = 100
+      h = 100
+  animateIO (InWindow "Realtime Chart"
                          (w,h)
                          (0,0))
                white
                (renderChart ref w h)
                controllerSetRedraw
-     cancel a
-
--- updateLabel :: Label -> String -> IO ()
--- updateLabel label title = labelSetText label title
-dataSeriesList
-  :: IntMap.IntMap MyData -> [(Int,Bid,Ask,Volume)]
-dataSeriesList =
-  fmap (\(_,d) -> (mdId d,mdBid d,mdAsk d,mdVolume d)) . IntMap.toList
+  cancel a
 
 -- http://stackoverflow.com/questions/5293898/how-to-pass-state-between-event-handlers-in-gtk2hs
 -- the below is not a working solution. Use MVar or TVar or IORef as
@@ -95,3 +72,8 @@ addAnother ds =
             (1 + mdVolume d))
     ds
   where (_,d) = IntMap.findMax ds
+
+dataSeriesList
+  :: IntMap.IntMap MyData -> [(Int,Bid,Ask,Volume)]
+dataSeriesList =
+  fmap (\(_,d) -> (mdId d,mdBid d,mdAsk d,mdVolume d)) . IntMap.toList
